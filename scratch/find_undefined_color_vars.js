fadeb6c9603819194929537948a9e83d411f2894
HARIKUMAR6 <harikumar.m.r.s.6@gmail.com>
2026-09-18 11:26:14 +0530

    new changes

--- scratch/find_undefined_color_vars.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const css = fs.readFileSync('css/style.css', 'utf8');
-
-const lines = css.split('\n');
-lines.forEach((l, idx) => {
-  if (l.includes('--color-brand') || l.includes('--color-accent-teal') || l.includes('--color-primary')) {
-    console.log(`${idx + 1}: ${l}`);
-  }
-});
--- scratch/inspect_section1_html.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('about.html', 'utf8');
-
-const s1 = html.slice(html.indexOf('id="about-hero"'), html.indexOf('id="about-approach"'));
-console.log('=== SECTION 1 HERO HTML ===');
-console.log(s1);
